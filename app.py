3d9b0beea848b2b4e7df960a6faddad680d37b57
Gladys Robles <105124485+lalylaly1227@users.noreply.github.com>
2022-07-10 15:32:42 -0400

    Module 9.5.2 completed

--- app.py
+++ app.py
@@ -1,5 +1,44 @@
-from flask import Flask
+# import is datetime, NumPy, and Pandas
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+# the dependencies we need for SQLAlchemy, 
+# which will help us access our data in the SQLite database.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from flask import Flask, jsonify
+
+# add the code to import the dependencies that we need for Flask.
+from sqlalchemy import create_engine, func
+
+# Access the SQLIte database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the database into our classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# create a variable for each of the classes so that we can reference them later
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create a session link from Python to our database
+session = Session(engine)
+
+# create a Flask application called "app."
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Hawaii Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
